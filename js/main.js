e47bbad988143b1f41d0daec57d67c2431ee94b7
JefG67 <jefguilpain@gmail.com>
2024-02-19 22:18:52 +0100

    finir compteur et ajouter des commentaires

--- js/main.js
+++ js/main.js
@@ -7,60 +7,67 @@ const taskCard = document.querySelector(".todoCard");
 
 const delBtn = document.querySelector(".delBtn");
 
-let count = 1 ;
+let count = 1 ; // pour déclarer que le compteur a 1 de base
 
 
 
-// console.log(taskCard)
 
+//fonction pour ajouter une carte 
 function addTask(){
-    // On récupère la classe todoCard et on le clone 
+    // On déclare une nouvelle variable pour  la classe todoCard et on la clone 
     const newTask = taskCard.cloneNode(true)
-    const newDelBtn =newTask.querySelector('.delBtn')
+    
     // On récupère le textArea de la card et on le remet par defaut 
-    const newTextArea = newTask.querySelector('.task')
+    const newTextArea = newTask.querySelector('.task') 
     newTextArea.value = "New Task";
+    
+    const newDelBtn = newTask.querySelector('.delBtn')
 
     newDelBtn.addEventListener('click', function(){
         deleteTask(newTask);
     });
 
-    // const tasksContainer =document.getElementById ("todoCards")
+    // ajout de la nouvelle carte 
     tasksContainer.appendChild(newTask)
     updateCount();
 }
 
-
-
-
-
-addBtn.addEventListener('click', function() {
-    addTask();
-}); //add a task on click
-
-
-
-
-
-
-delBtn.addEventListener('click', function(){
-    deleteTask(taskCard);
-}); 
-
-
-
+// fonction pour supprimer une carte 
 function deleteTask(task){
     task.remove();
     updateCount();
 }
 
 
+// Ajout d'un écouteur d'événements pour le bouton d'ajout d'une task
+addBtn.addEventListener('click', function() {
+    addTask();
+}); //add a task on click
 
-function count(){
+// Ajout d'un écouteur d'événements pour le bouton de suppression
+delBtn.addEventListener('click', function(){
+    deleteTask(taskCard);
+}); 
+
+
+
+
+
+
+
+
+
+
+function updateCount(){
     let newCount = document.querySelector ('#count')
-    if (count > 1){
-        newCount.textContent = taskCard + " cards";
-    } else{
-        newCount.textContent = taskCard + " card";
-    }
+    // if (count > 1){
+    //     newCount.textContent = taskCard + " cards";
+    // } else{
+    //     newCount.textContent = taskCard + " card";
+    // }
+
+
+
+    // newCount.textContent = 'Compteur (' + tasksContainer.children.length + ')';
 }
+
